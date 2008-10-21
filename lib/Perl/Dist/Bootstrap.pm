1ef560292952530521abb1fe94dcc8263e1940b1
Adam Kennedy <adamk@cpan.org>
2008-10-21 00:28:09 +0000

    Import of ADAMK/Perl-Dist-Strawberry-1.07 from CPAN.
    gitpan-cpan-distribution: Perl-Dist-Strawberry
    gitpan-cpan-version:      1.07
    gitpan-cpan-path:         ADAMK/Perl-Dist-Strawberry-1.07.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/Perl/Dist/Bootstrap.pm
+++ lib/Perl/Dist/Bootstrap.pm
@@ -1,13 +1,31 @@
 package Perl::Dist::Bootstrap;
 
-use 5.005;
+=pod
+
+=head1 NAME
+
+Perl::Dist::Bootstrap - A Perl distribution for building Perl distributions
+
+=head1 DESCRIPTION
+
+Bootstrap Perl is a subclass and variant of Strawberry Perl that installs
+into a different directory (C:\bootstrap) than Strawberry Perl so that
+it won't be "in the way" when building Strawberry and other Perls.
+
+It also comes prepackaged with a number of additional modules that are
+dependencies of Perl::Dist.
+
+=cut
+
+use 5.006;
 use strict;
-use base 'Perl::Dist::Strawberry';
+use Perl::Dist::Strawberry      ();
 use Perl::Dist::Util::Toolchain ();
 
-use vars qw{$VERSION};
+use vars qw{$VERSION @ISA};
 BEGIN {
-	$VERSION = '1.04';
+	$VERSION = '1.07';
+	@ISA     = 'Perl::Dist::Strawberry';
 }
 
 
@@ -20,7 +38,7 @@ BEGIN {
 # Apply some default paths
 sub new {
 	shift->SUPER::new(
-		app_id            => 'bootstrapperl',
+		app_id            => 'bootperl',
 		app_name          => 'Bootstrap Perl',
 		app_publisher     => 'Vanilla Perl Project',
 		app_publisher_url => 'http://vanillaperl.org/',
@@ -37,20 +55,31 @@ sub new {
 # Supports building multiple versions of Perl.
 sub output_base_filename {
 	$_[0]->{output_base_filename} or
-	'boostrap-perl-' . $_[0]->perl_version_human . '.1';
+	'bootstrap-perl-' . $_[0]->perl_version_human . '.3';
 }
 
-sub install_perl_5100 {
-	my $self = shift;
-	$self->SUPER::install_perl_5100(@_);
 
-	# Install the vanilla CPAN::Config
-	$self->install_file(
-		share      => 'Perl-Dist-Strawberry bootperl_CPAN_Config_5100.pm',
-		install_to => 'perl/lib/CPAN/Config.pm',
-	);
 
-	return 1;
+
+
+
+#####################################################################
+# Customisations for Perl assets
+
+sub patch_include_path {
+	my $self  = shift;
+
+	# Find the share path for this distribution
+	my $share = File::ShareDir::dist_dir('Perl-Dist-Strawberry');
+	my $path  = File::Spec->catdir( $share, 'bootstrap' );
+	unless ( -d $path ) {
+		die("Directory $path does not exist");
+	}
+
+	# Prepend it to the default include path
+	return [ $path,
+		@{ $self->SUPER::patch_include_path },
+	];
 }
 
 sub install_perl_modules {
@@ -84,9 +113,41 @@ sub install_perl_modules {
 		Test::LongString
 		Probe::Perl
 		Module::Install
+		Perl::Dist
 	} );
 
 	return 1;
 }
 
 1;
+
+=pod
+
+=head1 SUPPORT
+
+Bugs should be reported via the CPAN bug tracker at
+
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Perl-Dist-Strawberry>
+
+Please note that B<only> bugs in the distribution itself or the CPAN
+configuration should be reported to RT. Bugs in individual modules
+should be reported to their respective distributions.
+
+For more support information and places for discussion, see the
+Strawberry Perl Support page L<http://strawberryperl.com/support.html>.
+
+=head1 AUTHOR
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2007 - 2008 Adam Kennedy.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=cut
